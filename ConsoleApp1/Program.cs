9a673ec4d5426050832b5097596aa9e39ee744fe
stud <stud@I21013401494>
2022-11-02 13:08:15 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace FirstApp
+{
+    public class Logic
+    {
+        public static string removDuplicat(string word1)
+        {
+            int size = word1.Length;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i != j)
+                    {
+                        if (word1[i] == word1[j])
+                        {
+                            word1 = word1.Remove(j, 1);
+                            size--;
+                        }
+
+                    }
+                }
+            }
+            return word1;
+        }
+        public static string Task(string word1, string word2)
+        {
+            string word = "";
+            bool flag;
+            word1 = Logic.removDuplicat(word1);
+            for (int i = 0; i < word1.Length; i++)
+            {
+                flag = true;
+                for (int j = 0; j < word2.Length; j++)
+                {
+                    if (word1[i] == word2[j])
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag == true)
+                {
+                    word += "нет ";
+                }
+                else
+                {
+                    word += "да ";
+                }
+            }
+            return word;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое слово: ");
+            string word1 = Console.ReadLine();
+            Console.Write("Введите второе слово: ");
+            string word2 = Console.ReadLine();
+            string word3 = Logic.Task(word1, word2);
+            Console.WriteLine(word3);
+        }
+    }
+}
---
+++ ConsoleApp1Tests/LogicTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetTest1()
+        {
+            string word1 = "процессор";
+            string word2 = "информация";
+            string massage = Logic.Task(word1, word2);
+            Assert.AreEqual("нет да да да нет нет ", massage);
+        }
+        [TestMethod()]
+        public void Getest2()
+        {
+            string word1 = "кредитор";
+            string word2 = "редиска";
+            string massage = Logic.Task(word1, word2);
+            Assert.AreEqual("да да да да да нет нет ", massage);
+        }
+        [TestMethod()]
+        public void GetTest3()
+        {
+            string word1 = "расписка";
+            string word2 = "сенсор";
+            string massage = Logic.Task(word1, word2);
+            Assert.AreEqual("да нет да нет нет нет ", massage);
+        }
+        [TestMethod()]
+        public void GetTest4()
+        {
+            string word1 = "привет";
+            string word2 = "вектор";
+            string massage = Logic.Task(word1, word2);
+            Assert.AreEqual("нет да нет да да да ", massage);
+        }
+
+    }
+}
